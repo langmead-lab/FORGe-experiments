e75c9b5f7d6af4ae230425d2cbfef55df2aa2902
Mark Pritt <mpritt4@jhu.edu@login-node02.cm.cluster>
2018-01-11 12:50:03 -0500

    Improved scripts

--- scripts/run_temp.sh
+++ scripts/run_temp.sh
@@ -8,44 +8,11 @@ export SCRIPT_HOME=/home-3/mpritt4@jhu.edu/work/jacob/vis-experiments/scripts
 #./add_read_info.sh $EXP_HOME
 
 cd $EXP_HOME
-mkdir -p hisat_snps
-mkdir -p hisat_indexes
-
-mkdir -p aligned/$NAME
 
 export LEN=100
 export NAME=NA12878
 
-export MODE=popcov
-export PREFIX=chr9_all_${MODE}_l${LEN}
-rm -f ${PREFIX}.tsv
-printf "Pct\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.tsv
-rm -f ${PREFIX}.strat_snp.tsv
-printf "Pct\tSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_snp.tsv
-rm -f ${PREFIX}.strat_rare.tsv
-printf "Pct\tRareSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_rare.tsv
-rm -f ${PREFIX}.strat_snp.tsv
-printf "Pct\tDelSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_del.tsv
-rm -f ${PREFIX}.strat_region.tsv
-printf "Pct\tRegion\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_region.tsv
-sbatch $SCRIPT_HOME/build.sbatch --export=MODE,LEN,NAME,PREFIX
-#sbatch $SCRIPT_HOME/build_lrgmem.sbatch --export=MODE,LEN,NAME,PREFIX
-
-export MODE=popcov_blowup
-export PREFIX=chr9_all_${MODE}_l${LEN}
-rm -f ${PREFIX}.tsv
-printf "Pct\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.tsv
-rm -f ${PREFIX}.strat_snp.tsv
-printf "Pct\tSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_snp.tsv
-rm -f ${PREFIX}.strat_rare.tsv
-printf "Pct\tRareSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_rare.tsv
-rm -f ${PREFIX}.strat_snp.tsv
-printf "Pct\tDelSNPs\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_del.tsv
-rm -f ${PREFIX}.strat_region.tsv
-printf "Pct\tRegion\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_region.tsv
-sbatch $SCRIPT_HOME/build.sbatch --export=MODE,LEN,NAME,PREFIX
-
-export MODE=amb
+export MODE=amb100_max15
 export PREFIX=chr9_all_${MODE}
 rm -f ${PREFIX}.tsv
 printf "Pct\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.tsv
@@ -60,7 +27,7 @@ printf "Pct\tRegion\tTotal\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.strat_
 sbatch $SCRIPT_HOME/build.sbatch --export=MODE,LEN,NAME,PREFIX
 #sbatch $SCRIPT_HOME/build_lrgmem.sbatch --export=MODE
 
-export MODE=amb_blowup
+export MODE=amb_blowup100_max15
 export PREFIX=chr9_all_${MODE}
 rm -f ${PREFIX}.tsv
 printf "Pct\tHap\tAligned\tCorrect\tOverall\n" > ${PREFIX}.tsv
